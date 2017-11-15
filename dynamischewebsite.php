0242fa11010a126038c0c0a8bf64ba9b2e154a6b
H-Lal <2003255@talnet.nl>
2017-11-15 13:23:00 +0100

    Beasties en Dynamische website af.

--- dynamischewebsite.php
+++ dynamischewebsite.php
@@ -5,3 +5,30 @@
  * Date: 23-10-2017
  * Time: 14:06
  */
+
+$berichten = array();
+
+if (!isset($_GET["naam"]) && !isset($_GET["adres"]) && !isset($_GET["email"]) && !isset($_GET["password"])) {
+    $berichten[] = "Vul A.U.B. het formulier in.";
+} elseif (strlen($_GET["naam"]) < 0) {
+    $berichten[] = "Vul A.U.B. je naam in.";
+} elseif (strlen($_GET["adres"]) < 0) {
+    $berichten[] = "Vul A.U.B. je adres in.";
+} elseif (strlen($_GET["wachtwoord"]) < 0) {
+    $berichten[] = "Vul A.U.B. een wachtwoord in.";
+}
+if (!count($berichten) == 0 ) {
+    //berichten weergeven
+    $html = "<ul>";
+    foreach ($berichten as $bericht) {
+        $html .= "<li>" .$bericht . "</li>";
+    }
+    $html .= "<ul>";
+} else{
+    echo "Deze gegevens heb je ingevuld". "<br> <hr>";
+    echo $_GET["naam"]. "<br> <hr>";
+    echo $_GET["adres"]. "<br> <hr>";
+    echo $_GET["email"]. "<br> <hr>";
+    echo $_GET["wachtwoord"]. "<br> <hr>";
+}
+
---
+++ login.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vinci
+ * Date: 10-11-2017
+ * Time: 08:39
+ */
+session_start();
+
+$users = array(
+    "piet@worldonline.nl"=> array("pwd" => "doetje123", "rol" => "Gebruiker"),
+    "klaas@carpets.nl"=> array("pwd" => "snoepje777", "rol" => "Administrator"),
+    "truushendriks@wegweg.nl"=> array("pwd" => "arkiearkie201", "rol" => "Gebruiker")
+);
+if(isset($_GET["loguit"])){
+    $_SESSION = array();
+    session_destroy();
+}
+if(isset($_POST['knop'])
+    && isset($users[$_POST['email']])
+    && $users[$_POST["email"]]["pwd"] == $_POST ['pwd'] ){
+    $_SESSION["user"] = array("email" => $_POST["email"],
+                            "pwd" => $users [$_POST["email"]]['pwd'],
+                            "rol" => $users [$_POST["email"]]['rol']);
+    $message = "Welkom ".$_SESSION["user"]["email"]." met de rol ". $_SESSION["user"]["rol"];
+
+} else{
+    $message = "Inloggen";
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Login</title>
+  <meta charset="utf-8">
+</head>
+<style>
+body{
+    text-align: center;
+    }
+</style>
+<body>
+<h1><?php echo $message; ?></h1>
+<form action= "<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <hr>
+Email <br>
+    <input type="email" name="email" value="">
+    <br><hr>
+Wachtwoord <br>
+    <input type="password" name="pwd" value="">
+    <br><hr>
+    <button type="submit" name="knop">Submit</button>
+    <br><hr>
+</form>
+<p><a href="login.php?loguit"</a>Uitloggen</p>
+<p><a href="website.php"</a>website</p>
+
+</body>
+</html>
